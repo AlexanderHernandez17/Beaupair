19952df73186fad86a9de6078d3614d5ee7122ee
alexanderhernandez17 <alexander.hdez.2001gmail.com>
2025-01-19 15:46:56 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Ignora los errores y advertencias de ESLint en Vercel
+    },
+  }
+  
+  module.exports = nextConfig
